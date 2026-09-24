9d5235723c47f656a109bf03e473f8e57df39d7e
Miruo Lin <90869506+miruomoo@users.noreply.github.com>
2026-09-24 13:27:42 -0400

    Add: lru-cache - submission-7

---
+++ Data Structures & Algorithms/lru-cache/submission-7.py
@@ -0,0 +1,20 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key, last=False)
+            return self.cache[key]
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key, last=False)
+        if len(self.cache) > self.cap:
+            self.cache.popitem()
+
+        
